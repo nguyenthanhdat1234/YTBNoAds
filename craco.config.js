2798cb37e9a72d88a4f664f2202a67122cb53a68
nguyenthanhdat1234 <datnguyen.010802@gmail.com>
2025-08-19 19:24:29 +0700

    fix: fix error command vercel

--- craco.config.js
+++ craco.config.js
@@ -7,10 +7,29 @@ module.exports = {
   },
   webpack: {
     configure: (webpackConfig) => {
-      // Disable source maps in development for faster builds
+      // Disable source maps for faster builds
       if (process.env.NODE_ENV === 'development') {
         webpackConfig.devtool = false;
       }
+
+      // Production optimizations
+      if (process.env.NODE_ENV === 'production') {
+        webpackConfig.devtool = false;
+        webpackConfig.optimization = {
+          ...webpackConfig.optimization,
+          splitChunks: {
+            chunks: 'all',
+            cacheGroups: {
+              vendor: {
+                test: /[\\/]node_modules[\\/]/,
+                name: 'vendors',
+                chunks: 'all',
+              },
+            },
+          },
+        };
+      }
+
       return webpackConfig;
     },
   },
